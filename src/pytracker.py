c011c3014c881eec63ac4b179c37f574ea8e5aaa
David Mulder <dmulder@suse.com>
2019-11-07 14:15:28 -0700

    Add save capability (for adding to work report)

--- src/pytracker.py
+++ src/pytracker.py
@@ -16,7 +16,7 @@ class TaskNotify:
         self.work = []
         self.load_work()
         if len(self.work) > 0:
-            self.request_work(self.work[-1])
+            self.request_work(self.work[0])
         else:
             self.request_task()
 
@@ -31,7 +31,7 @@ class TaskNotify:
         env = os.environ
         env['EDITOR'] = which('cat')
         out, _ = Popen([which('re')], stdout=PIPE).communicate()
-        return out.decode()
+        return '\n'.join(out.decode().split('\n')[2:])
 
     def load_work(self):
         out = self.__dump_re()
@@ -42,22 +42,46 @@ class TaskNotify:
         self.work = [line.strip()[1:].strip() for line in out.split('\n') if len(line.strip()) > 0 and line.strip()[0] == bullet]
 
     def HandleDoNothing(self, obj, name):
-        pass
+        self.notifications.pop()
 
     def HandleEditTask(self, obj, name):
-        self.__modify_re(self.__dump_re())
+        self.notifications.pop()
+        self.__modify_re('Modify', self.__dump_re())
 
     def HandleFinishedTask(self, obj, name):
+        self.notifications.pop()
         self.request_task()
 
     def HandleAddTask(self, obj, name):
-        self.__modify_re('')
+        self.notifications.pop()
+        self.__modify_re('Add', '', append=True)
 
-    def __modify_re(self, text):
-        self.windows.append(Gtk.Window(title='Task Tracker'))
+    def __save_re(self, obj, append, textbuffer):
+        s = textbuffer.get_start_iter()
+        e = textbuffer.get_end_iter()
+        text = textbuffer.get_text(s, e, True)
+        if append:
+            text = text.replace('\n', ' ').strip()
+            Popen([which('re'), text], stdout=PIPE, stderr=PIPE).wait()
+        else:
+            pass
+
+    def __modify_re(self, title, text, append=False):
+        self.windows.append(Gtk.Window())
         win = self.windows[-1]
         win.set_default_size(500, 350)
         win.connect("destroy", Gtk.main_quit)
+
+        # Header
+        hb = Gtk.HeaderBar()
+        hb.set_show_close_button(True)
+        hb.props.title = 'Task Tracker - %s' % title
+        win.set_titlebar(hb)
+        save = Gtk.Button()
+        save.set_label('Save')
+        hb.pack_end(save)
+
+        # Main Document
         scrolledwindow = Gtk.ScrolledWindow()
         scrolledwindow.set_hexpand(True)
         scrolledwindow.set_vexpand(True)
@@ -65,6 +89,7 @@ class TaskNotify:
         textview = Gtk.TextView()
         textbuffer = textview.get_buffer()
         textbuffer.set_text(text)
+        save.connect('clicked', self.__save_re, append, textbuffer)
         scrolledwindow.add(textview)
         win.show_all()
         self.load_work()
